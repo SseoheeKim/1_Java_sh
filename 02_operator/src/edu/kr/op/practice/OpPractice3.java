16eff27ff8b36ceb0cd41c991e309f6396759aef
Sseohee <shuxi91@naver.com>
2022-08-08 13:50:55 +0900

    실습문제3.4

--- 02_operator/src/edu/kr/op/practice/OpPractice3.java
+++ 02_operator/src/edu/kr/op/practice/OpPractice3.java
@@ -1,10 +1,15 @@
 package edu.kr.op.practice;
 
+import java.util.Scanner;
+
 public class OpPractice3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		System.out.print("정수입력 : ");
+		int input1 = sc.nextInt();
+		String result = input1 > 0 ? "양수입니다." : input1 == 0? "0 입니다." : "음수입니다.";
+		System.out.println(result);
+		sc.close();
 	}
-
 }
--- 02_operator/src/edu/kr/op/practice/OpPractice4.java
+++ 02_operator/src/edu/kr/op/practice/OpPractice4.java
@@ -1,10 +1,28 @@
 package edu.kr.op.practice;
 
+import java.util.Scanner;
+
 public class OpPractice4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		System.out.print("국어 : ");
+		int kor = sc.nextInt();
+		
+		System.out.print("영어 : ");
+		int eng = sc.nextInt();
+		
+		System.out.print("수학 : ");
+		int math = sc.nextInt();
+		
+		int totalSum = kor+eng+math;
+		double avg = totalSum / 3.0;
+		String result = kor >= 40 && eng >=40 && math >=40 || avg >= 60? "합격":"불합격";
+				
+		System.out.printf("합계 : %d\n", totalSum);
+		System.out.printf("평균 : %.1f\n", avg);
+		System.out.println(result);
+		sc.close();
 	}
 
 }
