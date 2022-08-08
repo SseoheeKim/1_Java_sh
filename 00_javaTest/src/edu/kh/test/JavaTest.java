8f0654619f0264e0d1ff5d6dec165d366fd3fd45
Sseohee <shuxi91@naver.com>
2022-08-08 13:28:44 +0900

    00_javaTest

---
+++ 00_javaTest/src/edu/kh/test/JavaTest.java
@@ -0,0 +1,21 @@
+package edu.kh.test;
+
+//class : 자바코드가 작성되는 영역
+public class JavaTest {
+	
+	//main메서드 : 자바 애플리케이션 실행을 위한 필수구문
+	public static void main(String[] args) {
+		
+		System.out.println("Hello, Java!");
+		System.out.println("println()은 한 줄 출력입니다.");
+
+		// 한 줄 주석: 컴파일러가 해석하지 않는 부분
+		
+		/*
+		 범위 주석(여러 줄 주석) 
+		 */
+		
+		System.out.println("git");
+		System.out.println("연습하기");
+	}
+}
